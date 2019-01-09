75558203c91e5a6c95d5bfbf4aec0f86c59961e7
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2019-01-09 22:53:05 +0200

    first commit/ twitter search

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key: 'P3JlGVSeYHuSh0eawDlsRY1yr',
+    consumer_secret: 'mUrtTysUtcb4DkfJUUvUz9ywjI7VhchFfUtaWFVvH93jllc0w3',
+    access_token: '1083085278527524865-r4lnGpfJ0XxJlMic0RBbpvVTWb80Vf',
+    access_token_secret: 'uljPSzqWjTurzcg5uIepxRZZBcSRPp20icCd5Do8vxRmR',
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+    strictSSL: true, // optional - requires SSL certificates to be valid.
+}
